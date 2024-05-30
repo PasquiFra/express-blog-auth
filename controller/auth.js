9f5f74586dcc852b5fee82a693b09eeb08a7b0da
Your Name <you@example.com>
2024-05-30 10:19:03 +0200

    fix

--- controller/auth.js
+++ controller/auth.js
@@ -30,14 +30,14 @@ const authentication = (req, res, next) => {
 
     //verifico che il token sia valido hashandolo con la mia private key
     //? Il metodo verify mi restituirà il payload del token, contenente i dati codificati nel token al momento della sua creazione.
-    jwt.verify(token, process.env.AUTH_KEY, (err, user) => {
+    jwt.verify(token, process.env.AUTH_KEY, (err, payload) => {
         if (err) {
             err.message == "jwt expired" ? err.message = "token scaduto" : err.message = "Autenticazione fallita, effettua il login"
             err.status = 401;
             return errorDetector(err, req, res, next)
         }
         //se l'user che ricevo nella decodifica corrisponde lo assegno nella request
-        req.user = user;
+        req.user = payload;
 
         next();
     });
--- middlewares/errorsDetector.js
+++ middlewares/errorsDetector.js
@@ -1,6 +1,6 @@
 module.exports = (err, req, res, next) => {
-    res.format({
-        //html: () => res.status(err.status).send(err.message),
+    return res.format({
+        html: () => res.status(err.status).send(`<h1>${err.message}</h1>`),
         json: () => res.status(err.status).json({ status: err.status, error: err.message })
     });
 }
